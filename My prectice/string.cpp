586c94ba2a2892c9c8c1ce1570ebdb94d2e48319
Ashraful Islam <102016365+Ashraful093@users.noreply.github.com>
2022-07-23 00:36:13 +0600

    Add files via upload

---
+++ My prectice/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter your mobile number:"<< flush;
+    string input;
+    cin>> input;
+    cout << "You entered :" << input << endl;
+
+    return 0;
+}
