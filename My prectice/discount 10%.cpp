a66b207e076d3a833d8c0868e36981bab79cf218
Ashraful Islam <102016365+Ashraful093@users.noreply.github.com>
2022-07-23 01:24:53 +0600

    Add files via upload

---
+++ My prectice/discount 10%.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int quantity,price;
+	cout << "Enter quantity" << endl;
+	cin >> quantity;
+	price = quantity*100;
+	if (price>1000){
+		cout << "Total cost is " << price-(price*.1) << endl;
+	}
+	else{
+		cout << "Total cost is " << price << endl;
+	}
+    return 0;
+}
